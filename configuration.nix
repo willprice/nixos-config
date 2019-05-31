e7a35cf56f83f5062619710766d29f84e43a0371
Will Price <will.price94@gmail.com>
2019-05-31 18:24:45 +0100

    Update

--- configuration.nix
+++ configuration.nix
@@ -6,87 +6,104 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
+      ./packages.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.efiSupport = false;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "orchid"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # VBox only
+  # fsck will fail under vbox and cause a boot to hang, so turn it off
+  boot.initrd.checkJournalingFS = false;
+  # End VBox only
 
-  # Select internationalisation properties.
+  networking.hostName = "nixos";
+  networking.wireless.enable = true;
+
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "uk";
     defaultLocale = "en_GB.UTF-8";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/London";
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    wget
-    vim
-    firefox
-    git
-    stow
-    tree
-    fish
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
-  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
+  programs.vim.defaultEditor = true;
+  #programs.nylas-mail.enable = true;
+  programs.npm.enable = true;
+  #programs.nm-applet.enable = true;
+  programs.less.enable = true;
+  #programs.iotop.enable = true;
+  programs.iftop.enable = true;
+  programs.java.enable = true;
+  programs.gphoto2.enable = true;
+  services.mpd = {
+    enable = true;
+    startWhenNeeded = true;
+  };
 
-  # Enable the OpenSSH daemon.
+  services.acpid.enable = true;
+  services.redshift = {
+    enable = true;
+    latitude = "51.4545";
+    longitude = "-2.5879";
+  };
+
   services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable the X11 windowing system.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
   services.xserver.enable = true;
   services.xserver.layout = "gb";
   # services.xserver.xkbOptions = "eurosign:e";
 
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.mate.enable = true;
+  services.xserver.windowManager.xmonad = {
+    enable = true;
+    enableContribAndExtras = true;
+    extraPackages = haskellPackages: [
+      haskellPackages.xmonad-contrib
+      haskellPackages.xmonad-extras
+      haskellPackages.xmonad
+    ];
+  };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.will = {
+  # Don't forget to set a password with ‘passwd’.
+  users.users.will = {
     isNormalUser = true;
-    uid = 1001;
+    home = "/home/will";
+    uid = 1000;
+    extraGroups = [ "wheel" "networkmanager" ];
   };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
+  system.stateVersion = "18.09";
+
   system.autoUpgrade.enable = true;
 
 }
